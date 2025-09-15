9e9e14c0f18ff250045caa3468d8b9df30e47176
Antoine Payette-Toupin <anpayb@l4712-05.info.polymtl.ca>
2025-09-15 19:42:41 -0400

    removed old code

--- source/pipeline-tbb.cpp
+++ source/pipeline-tbb.cpp
@@ -77,34 +77,6 @@ class saveFilter : public tbb::filter_t<image_t*, void>{
 
 int pipeline_tbb(image_dir_t* image_dir) {
 
-    // tbb::filter_t load_filter = tbb::make_filter<void, image_t*>(tbb::filter::serial,
-    //     [&](tbb::flow_control& fc)-> image_t*{
-    //         image_t* imageTemp = image_dir_load_next(image_dir);
-    //         return imageTemp;
-    //     });
-
-    //     tbb::filter_t scale_filter = tbb::make_filter<image_t*,image_t*>(tbb::filter::parallel,
-    //     [](image_t* image){
-    //         image_t* tempImg = filter_scale_up(image, 2);
-    //         image_destroy(image);
-    //         return tempImg;
-    //     });
-
-    // tbb::filter_t sobel_filter = tbb::make_filter<image_t*,image_t*>(tbb::filter::parallel,
-    //     [](image_t* image){
-    //         image_t* tempImg = filter_sobel(image);
-    //         image_destroy(image);
-    //         return tempImg;
-    //     });
-
-    // tbb::filter_t save_filter = tbb::make_filter<image_t*, void>(tbb::filter::serial_out_of_order,
-    //     [&](image_t* image){
-    //         image_dir_save(image_dir, image);
-    //         printf(".");
-    //         fflush(stdout);
-    //         image_destroy(image);
-    //     });
-
     // tbb::filter_t myFilter = tbb::make_filter<Type1, Type2>(tbb::filter::mode, functor); where functor operator() maps Type1 to Type2
     tbb::filter_t load_filter = tbb::make_filter<void, image_t*>(tbb::filter::serial, loadFilter(image_dir));
     tbb::filter_t scale_filter = tbb::make_filter<image_t*, image_t*>(tbb::filter::parallel, scaleFilter());
