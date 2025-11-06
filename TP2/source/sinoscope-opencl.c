6676360509d704d4bcf0d013cf34a2b1951fb12e
Antoine Payette-Toupin <anpayb@l4712-16.info.polymtl.ca>
2025-11-06 18:29:33 -0500

    Finished draft program, not working

--- TP2/source/sinoscope-opencl.c
+++ TP2/source/sinoscope-opencl.c
@@ -3,48 +3,82 @@
 #include "log.h"
 #include "sinoscope.h"
 
+
+typedef struct sinoscope_params {
+    float interval_inverse;
+    float time;
+    float max;
+    float phase0;
+    float phase1;
+    float dx;
+    float dy;
+
+    unsigned int width;
+    unsigned int height;
+    unsigned int taylor;
+    unsigned int interval;
+} sinoscope_params_t;
+
+
 int sinoscope_opencl_init(sinoscope_opencl_t* opencl, cl_device_id opencl_device_id, unsigned int width,
 			  unsigned int height) {
 
-	cl_int* ret;
+	cl_int ret;
 
-	opencl->context = clCreateContext(NULL, 1, &opencl_device_id,NULL,NULL,ret);
+	// Setting device ID
+	opencl->device_id = opencl_device_id;
+
+	// Create the compute context
+	opencl->context = clCreateContext(NULL, 1, &opencl_device_id,NULL,NULL,&ret);
 	if (ret != CL_SUCCESS) {
 		LOG_ERROR("clCreateContext failed (%d)", ret);
 		goto fail_exit;
 	}
 
-	size_t size = 3 * width * height; // Is this in bytes?? (i think so) on char is one byte so 3 * 1byte * nb pixels
-	opencl->buffer = clCreateBuffer(opencl->context, CL_MEM_READ_WRITE,size,NULL,ret); // host_ptr? // Could also be a clCreateImage2D
-	if (ret != CL_SUCCESS) {
-		LOG_ERROR("clCreateBuffer failed (%d)", ret);
-		goto fail_exit;
-	}
-
-	opencl->queue = clCreateCommandQueue(opencl->context, opencl_device_id, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE ,ret);
+	// Create a command queue
+	opencl->queue = clCreateCommandQueue(opencl->context, opencl_device_id, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE ,&ret);
 	if (ret != CL_SUCCESS){
 		LOG_ERROR("clCreateCommandQueue failed (%d)", ret);
 		goto fail_exit;
 	}
 	int opencl_load_kernel_code(char** code, size_t* len);
 	
+	// Create the compute program from the source buffer
 	char* sinoscope_code = NULL;
 	size_t sinoscope_code_len = 0;
 	opencl_load_kernel_code(&sinoscope_code, &sinoscope_code_len);
 	printf("This is the kernel code length: %zu\n", sinoscope_code_len);
 	printf("This is the code I think: %s\n", sinoscope_code);
 	cl_program program = clCreateProgramWithSource(opencl->context, 1, (const char**)&sinoscope_code,
-						 &sinoscope_code_len, ret);
+						 &sinoscope_code_len, &ret);
 	if (ret != CL_SUCCESS){
 		LOG_ERROR("clCreateProgramWithSource failed (%d)", ret);
 		goto fail_exit;
 	}
+		
+	// Create the arrays in shared memory for calculation
+	size_t size = 3 * width * height;
+	opencl->buffer = clCreateBuffer(opencl->context, CL_MEM_READ_WRITE,size,NULL,&ret); // host_ptr? // Could also be a clCreateImage2D
+	if (ret != CL_SUCCESS) {
+		LOG_ERROR("clCreateBuffer failed (%d)", ret);
+		goto fail_exit;
+	}
 
-	ret = clBuildProgram(program,1,opencl_device_id,NULL,NULL,NULL);
+	// Build the program executable
+	cl_device_id devices[] = {opencl_device_id};
+	ret = clBuildProgram(program,1,devices,NULL,NULL,NULL);
 	if (ret != CL_SUCCESS){
 		LOG_ERROR("clBuildProgram failed (%d)", ret);
 		goto fail_exit;
 	}
+
+	// Create the compute kernel in the program we wish to run
+	opencl->kernel = clCreateKernel(program, "kernel_sinoscope", &ret);
+	if (ret != CL_SUCCESS){
+		LOG_ERROR("clCreateKernel failed (%d)", ret);
+		goto fail_exit;
+	}
+
 
 	return 0;
 
@@ -81,7 +115,49 @@ int sinoscope_image_opencl(sinoscope_t* sinoscope) {
         goto fail_exit;
     }
 
+	sinoscope_params_t* params = malloc(sizeof(sinoscope_params_t));
+	// float
+	params->interval_inverse = sinoscope->interval_inverse;
+	params->time = sinoscope->time;
+	params->max = sinoscope->max;
+	params->phase0 = sinoscope->phase0;
+	params->phase1 = sinoscope->phase1;
+	params->dx = sinoscope->dx;
+	params->dy = sinoscope->dy;
+
+	//uint
+	params->width = sinoscope->width;
+	params->height = sinoscope->height;
+	params->taylor = sinoscope->taylor;
+	params->interval = sinoscope->interval;
+
+
+
+	// Exécution du kernel OpenCl
+	cl_int ret;
+	ret = clSetKernelArg(sinoscope->opencl->kernel, 0, sizeof(cl_mem),  (void*)&sinoscope->opencl->buffer);
+	if (ret != CL_SUCCESS){
+		LOG_ERROR("clSetKernelArg 0 failed (%d)", ret);
+		goto fail_exit;
+	}
+	ret = clSetKernelArg(sinoscope->opencl->kernel, 1, sizeof(sinoscope_params_t),  (void*)&params);
+	if (ret != CL_SUCCESS){
+		LOG_ERROR("clSetKernelArg 1 failed (%d)", ret);
+		goto fail_exit;
+	}
+
+	// Execute the kernel over the entire range
+	size_t global_work_size[] = { sinoscope->buffer_size };
+	size_t local_work_size[] = { sinoscope->buffer }; 
+
+	ret = clEnqueueNDRangeKernel(sinoscope->opencl->queue, sinoscope->opencl->kernel, 1, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+
 	
+	if (ret != CL_SUCCESS){
+		LOG_ERROR("clEnqueueNDRangeKernel failed (%d)", ret);
+		goto fail_exit;
+	}
+
 
 	return 0;
 
