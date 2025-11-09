8672ad3b51535b348b80c2127afb6ae83c8739a4
TPerronTouchette <thomas.perron-touchette@polymtl.ca>
2025-11-09 15:03:36 -0500

    feat: add the main omp pragma

--- TP2/source/sinoscope-openmp.c
+++ TP2/source/sinoscope-openmp.c
@@ -12,13 +12,13 @@ int sinoscope_image_openmp(sinoscope_t* sinoscope) {
         LOG_ERROR_NULL_PTR();
         goto fail_exit;
     }
-
+	#pragma omp parallel for simd schedule(dynamic)
     for (int i = 0; i < sinoscope->width; i++) {
         for (int j = 0; j < sinoscope->height; j++) {
             float px    = sinoscope->dx * j - 2 * M_PI;
             float py    = sinoscope->dy * i - 2 * M_PI;
             float value = 0;
-
+			
             for (int k = 1; k <= sinoscope->taylor; k += 2) {
                 value += sin(px * k * sinoscope->phase1 + sinoscope->time) / k;
                 value += cos(py * k * sinoscope->phase0) / k;
